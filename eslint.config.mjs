c27352b6037fb859cbb61256a973cffb6db1bd1d
Rehan Khan <rehanansari0342@gmail.com>
2025-03-13 16:56:01 +0500

    remove eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,13 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // Custom ESLint rules
-  {
-    rules: {
-      "@typescript-eslint/no-unused-vars": "off", // Disables unused variable errors
-    },
-  },
 ];
 
 export default eslintConfig;
